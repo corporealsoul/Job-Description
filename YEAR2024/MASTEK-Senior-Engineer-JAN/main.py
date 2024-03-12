7237caae3158dbabdcbe54fcacf508386d22ec86
Anup Kumar Mondal <bca.anup@gmail.com>
2024-03-12 22:26:35 +0530

    MASTEK-Senior-Engineer-JAN

--- YEAR2024/MASTEK-Senior-Engineer-JAN/main.py
+++ YEAR2024/MASTEK-Senior-Engineer-JAN/main.py
@@ -1,5 +1,25 @@
+# from lib.logic import wiki
+
+# result = wiki()
+# print(result)
+
+#
+
+from fastapi import FastAPI
+import uvicorn
 from lib.logic import wiki
 
-result = wiki()
+app = FastAPI()
 
-print(result)
+@app.get("/")
+async def root():
+    return {"Messege": "Hey Project"}
+
+@app.get("add/{num1}/{num2}")
+async def add(num1: int, num2: int):
+    """Add two numbers together"""
+    total = num1 + num2
+    return{"Total": total}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
